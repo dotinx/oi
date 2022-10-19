87c8b2ae8eaf2221bf4c9ac9ae153ed7f8e67689
guangyao-d <guangyao@gmx.com>
2022-10-19 10:28:25 +0800

    P4789没想出来 先等等罢（

---
+++ codes/practice/code/P4789.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXN = pow(2,30);
+int n,k,t;
+int a[MAXN],
+int main(){
+  scanf("%d%d",&n,&k);
+  for(int i = 0;i < n;i++){
+    scanf("%d",&t);
+    a.push(t);
+  }
+  while(true){
+    int top = a.top();a.pop();
+    b.push
+  }
+  return 0;
+}
