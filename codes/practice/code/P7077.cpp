c82dc130d84af4a6672177e1123f729393b84828
guangyao-d <guangyao@gmx.com>
2022-10-24 22:10:11 +0800

    没整完

--- codes/practice/code/P7077.cpp
+++ codes/practice/code/P7077.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-//(a + c) * b = a * b + c * b
-//((a + c) * b + e) *d = a * b * d + b * c * d + e * d
 typedef long long int ll;
-ll datas[200000];
-struct func{
-  int operator;
-  int object;
-  ll value;
-};
+const int maxn = 100010,mod = 998244353;
+
+int n,m;
+
+int cnt[maxn];//cnt -> 函数的执行次数
+int data[maxn],type[maxn];//data -> 数据串，type ->操作
+int mule[maxn];//对操作2的因数
+int add[maxn],pos[maxn];//对操作1的加数与位置
+
+int main(){
+  scanf("%d",&n);
+  for(int i = 1;i <= n;i++) scanf("%d",data + i);
+  scanf("%d",&m);
+}
